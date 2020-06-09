7d9fe4f9e14c6db5e452bbd54a570e3c048b1dd9
Edson J Vargas <edson.jhair@yahoo.com>
2020-06-09 06:59:06 -0400

    Se agrega custom header

--- themes/altum/views/page/index.php
+++ themes/altum/views/page/index.php
@@ -2,8 +2,8 @@
 
 <?php require THEME_PATH . 'views/partials/ads_header.php' ?>
 
-<div class="container">
-    <nav aria-label="breadcrumb">
+<!-- <div class="container"> -->
+    <!-- <nav aria-label="breadcrumb">
         <small>
             <ol class="custom-breadcrumbs">
                 <li><a href="<?= url() ?>"><?= $this->language->index->breadcrumb ?></a> <i class="fa fa-angle-right"></i></li>
@@ -14,9 +14,9 @@
                 <li class="active" aria-current="page"><?= $this->language->page->breadcrumb ?></li>
             </ol>
         </small>
-    </nav>
+    </nav> -->
 
-    <div class="d-flex flex-column flex-md-row align-items-md-center justify-content-md-between">
+    <!-- <div class="d-flex flex-column flex-md-row align-items-md-center justify-content-md-between">
         <h1><?= $data->page->title ?></h1>
 
         <div class="d-print-none col-auto p-0 d-flex align-items-center">
@@ -26,8 +26,8 @@
                 <?= get_admin_options_button('page', $data->page->page_id) ?>
             <?php endif ?>
         </div>
-    </div>
-    <p class="text-muted mb-4">
+    </div> -->
+    <!-- <p class="text-muted mb-4">
         <span class="mr-3"><?= $data->page->description ?></span>
 
         <?php $estimated_reading_time = string_estimate_reading_time($data->page->content) ?>
@@ -38,11 +38,25 @@
             <?= $estimated_reading_time->minutes == 0 && $estimated_reading_time->seconds ? sprintf($this->language->page->estimated_reading_time, $estimated_reading_time->seconds . ' ' . $this->language->global->date->seconds) : null ?>
         </small>
         <?php endif ?>
-    </p>
+    </p> -->
+
+    <!-- <?= $data->page->content ?> -->
+
+    <!-- <div class="mt-4">
+        <small class="text-muted"><i class="fa fa-calendar"></i> <?= sprintf($this->language->page->last_date, \Altum\Date::get($data->page->last_date, 2)) ?></small>
+    </div> -->
+<!-- </div> -->
+
+<header id="pricing" class="header py-9">
+      <div class="d-flex align-items-center text-light">
+        <div class="container position-relative zi-1">
+          <div class="row align-items-center">
+            <div class="col-12 position-relative text-center zi-4">
+              <h2 class="fw-200"><?= $data->page->description ?></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
 
     <?= $data->page->content ?>
-
-    <div class="mt-4">
-        <small class="text-muted"><i class="fa fa-calendar"></i> <?= sprintf($this->language->page->last_date, \Altum\Date::get($data->page->last_date, 2)) ?></small>
-    </div>
-</div>
